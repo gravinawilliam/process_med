27ef68984ecb4f04dc621f617543902eda8cc244
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-21 01:36:50 -0300

    feat: created file for server

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import app from './app';
+import '../typeorm';
+
+dotenv.config();
+
+app.listen(process.env.API_PORT || 3000, () => {
+  if (process.env.NODE_ENV === 'dev') {
+    // eslint-disable-next-line no-console
+    console.log(`✅ OK ${process.env.API_PORT || 3000}`);
+  }
+});
