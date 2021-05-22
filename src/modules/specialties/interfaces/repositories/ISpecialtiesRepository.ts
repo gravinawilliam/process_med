7e95e9d723d6b84aab0ff88440b33e8176557ea1
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-21 23:19:52 -0300

    feat: added method find by name in interface specialties repository

--- src/modules/specialties/interfaces/repositories/ISpecialtiesRepository.ts
+++ src/modules/specialties/interfaces/repositories/ISpecialtiesRepository.ts
@@ -3,4 +3,5 @@ import ISpecialty from '../models/ISpecialty';
 
 export default interface ISpecialtiesRepository {
   create(specialty: ICreateSpecialtyDTO): Promise<ISpecialty>;
+  findByName(name: string): Promise<ISpecialty | undefined>;
 }
