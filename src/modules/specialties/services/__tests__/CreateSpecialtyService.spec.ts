5886aa7f9b1a2456a0c0b8fb1d976b1adf8215b9
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-21 23:31:29 -0300

    test: should not be able to create a new specialty with the same name

--- src/modules/specialties/services/__tests__/CreateSpecialtyService.spec.ts
+++ src/modules/specialties/services/__tests__/CreateSpecialtyService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeSpecialtiesRepository from '@modules/specialties/fakes/FakeSpecialtiesRepository';
 import CreateSpecialtyService from '../CreateSpecialtyService';
 
@@ -15,5 +16,16 @@ describe('Create Specialty', () => {
       name: 'Alergologia',
     });
     expect(specialty).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new specialty with the same name', async () => {
+    await fakeSpecialtiesRepository.create({
+      name: 'Alergologia',
+    });
+    await expect(
+      createSpecialty.execute({
+        name: 'Alergologia',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
