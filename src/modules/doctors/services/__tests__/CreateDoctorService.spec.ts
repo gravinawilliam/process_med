21227aada2cce28768766a6c5c8995b46e06a8b7
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-22 11:37:07 -0300

    test: should not be able to create a new doctor with the same cellphone

--- src/modules/doctors/services/__tests__/CreateDoctorService.spec.ts
+++ src/modules/doctors/services/__tests__/CreateDoctorService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeDoctorsRepository from '@modules/doctors/fakes/FakeDoctorsRepository';
 import CreateDoctorService from '../CreateDoctorService';
 
@@ -12,12 +13,32 @@ describe('Create Doctor', () => {
 
   it('must be able to create a new doctor', async () => {
     const doctor = await createDoctor.execute({
-      name: 'Alergologia',
+      name: 'William',
       cellPhone: '(32) 99833-8853',
       cep: '36503-312',
       crm: '33.225.11',
       landline: '(32) 3532-2280',
     });
     expect(doctor).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new doctor with the same cellphone', async () => {
+    await fakeDoctorsRepository.create({
+      name: 'William',
+      cellPhone: '(32) 99833-8853',
+      cep: '36503-312',
+      crm: '33.225.11',
+      landline: '(32) 3532-2280',
+    });
+
+    await expect(
+      createDoctor.execute({
+        name: 'Will',
+        cellPhone: '(32) 99833-8853',
+        cep: '42332-311',
+        crm: '33.534.12',
+        landline: '(32) 3532-3123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
