0097bb1286825a02c602d0da476151ff633e8314
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-22 01:03:42 -0300

    feat: created doctors repository with creation method using typeorm

---
+++ src/modules/doctors/infra/typeorm/repositories/DoctorsRepository.ts
@@ -0,0 +1,19 @@
+import ICreateDoctorDTO from '@modules/doctors/interfaces/dtos/ICreateDoctorDTO';
+import IDoctor from '@modules/doctors/interfaces/models/IDoctor';
+import IDoctorsRepository from '@modules/doctors/interfaces/repositories/IDoctorsRepository';
+import { getRepository, Repository } from 'typeorm';
+import Doctor from '../entities/Doctor';
+
+export default class DoctorsRepository implements IDoctorsRepository {
+  private ormRepository: Repository<IDoctor>;
+
+  constructor() {
+    this.ormRepository = getRepository(Doctor);
+  }
+
+  public async create(doctor: ICreateDoctorDTO): Promise<IDoctor> {
+    const doctorCreated = this.ormRepository.create(doctor);
+    await this.ormRepository.save(doctorCreated);
+    return doctorCreated;
+  }
+}
