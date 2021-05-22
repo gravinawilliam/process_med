92d8d26dd7640cee1a89f0a527b62d882878216f
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-22 01:07:17 -0300

    feat: controller created for create doctor

---
+++ src/modules/doctors/infra/http/controllers/DoctorsController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
+import { CREATED } from '@shared/constants/HttpStatusCode';
+import CreateDoctorService from '@modules/doctors/services/CreateDoctorService';
+
+export default class DoctorsController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { name, cellPhone, cep, crm, landline } = req.body;
+    const createDoctor = container.resolve(CreateDoctorService);
+    const response = await createDoctor.execute({
+      name,
+      cellPhone,
+      cep,
+      crm,
+      landline,
+    });
+    return res.status(CREATED).json(classToClass(response));
+  }
+}
