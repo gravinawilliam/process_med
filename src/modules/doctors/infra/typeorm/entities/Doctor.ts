bf9ebca9ba5d665119c87b18056d2f72c13fbf37
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-22 11:56:43 -0300

    feat: added encryption database in entity doctor

--- src/modules/doctors/infra/typeorm/entities/Doctor.ts
+++ src/modules/doctors/infra/typeorm/entities/Doctor.ts
@@ -1,6 +1,7 @@
 import { Exclude } from 'class-transformer';
 import { v4 } from 'uuid';
 import IDoctor from '@modules/doctors/interfaces/models/IDoctor';
+import EncryptionDataBase from '@shared/utils/EncryptionDataBase';
 import {
   Entity,
   Column,
@@ -17,28 +18,33 @@ export default class Doctor implements IDoctor {
   @Column({
     type: 'varchar',
     length: '120',
+    transformer: EncryptionDataBase,
   })
   name: string;
 
   @Column({
     type: 'varchar',
     unique: true,
+    transformer: EncryptionDataBase,
   })
   crm: string;
 
   @Column({
     type: 'varchar',
+    transformer: EncryptionDataBase,
   })
   landline: string;
 
   @Column({
     type: 'varchar',
     unique: true,
+    transformer: EncryptionDataBase,
   })
   cellPhone: string;
 
   @Column({
     type: 'varchar',
+    transformer: EncryptionDataBase,
   })
   cep: string;
 
