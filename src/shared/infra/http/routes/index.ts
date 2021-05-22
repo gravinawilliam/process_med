82c5d64fc1987a8e4b31ffa70a6416e252a8f294
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-21 23:06:01 -0300

    chore: added specialties route in routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import specialties from '@modules/specialties/infra/http/routes/specialties.routes';
 
 const routes = Router();
+
+routes.use('/specialties', specialties);
 
 export default routes;
