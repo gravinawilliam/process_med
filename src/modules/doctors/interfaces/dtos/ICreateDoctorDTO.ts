492135ad556fc6dbf59863ccf575ff0c1bb96844
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-22 20:55:35 -0300

    refactor: added atribute specialties in dto for create doctor'

--- src/modules/doctors/interfaces/dtos/ICreateDoctorDTO.ts
+++ src/modules/doctors/interfaces/dtos/ICreateDoctorDTO.ts
@@ -1,7 +1,10 @@
+import ISpecialty from '@modules/specialties/interfaces/models/ISpecialty';
+
 export default interface ICreateDoctorDTO {
   name: string;
   crm: string;
   landline: string;
   cellPhone: string;
   cep: string;
+  specialties: ISpecialty[];
 }
