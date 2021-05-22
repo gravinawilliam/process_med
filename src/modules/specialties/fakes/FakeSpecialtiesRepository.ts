2224884def4795d89005255370a1c8b7f3479738
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-21 23:16:55 -0300

    feat: fake created for specialties repository

---
+++ src/modules/specialties/fakes/FakeSpecialtiesRepository.ts
@@ -0,0 +1,20 @@
+import { v4 } from 'uuid';
+import Specialty from '../infra/typeorm/entities/Specialty';
+import ICreateSpecialtyDTO from '../interfaces/dtos/ICreateSpecialtyDTO';
+import ISpecialty from '../interfaces/models/ISpecialty';
+import ISpecialtiesRepository from '../interfaces/repositories/ISpecialtiesRepository';
+
+export default class FakeSpecialtiesRepository
+  implements ISpecialtiesRepository
+{
+  private specialties: ISpecialty[] = [];
+
+  public async create(specialty: ICreateSpecialtyDTO): Promise<ISpecialty> {
+    const userCreated = Object.assign(new Specialty(), {
+      id: v4(),
+      ...specialty,
+    });
+    this.specialties.push(userCreated);
+    return userCreated;
+  }
+}
