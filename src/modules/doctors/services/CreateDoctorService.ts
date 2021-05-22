0306a4ed44738355fef5a1daf525c7308693834a
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-22 11:36:44 -0300

    feat: added verification if the cellphone of the doctor already exists in the database

--- src/modules/doctors/services/CreateDoctorService.ts
+++ src/modules/doctors/services/CreateDoctorService.ts
@@ -1,3 +1,5 @@
+import { CONFLICT } from '@shared/constants/HttpStatusCode';
+import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
 import ICreateDoctorDTO from '../interfaces/dtos/ICreateDoctorDTO';
 import IDoctor from '../interfaces/models/IDoctor';
@@ -17,6 +19,13 @@ export default class CreateDoctorService {
     crm,
     landline,
   }: ICreateDoctorDTO): Promise<IDoctor> {
+    const cellPhoneExists = await this.doctorsRepository.findByCellPhone(
+      cellPhone,
+    );
+    if (cellPhoneExists) {
+      throw new AppError('CellPhone name already exists.', CONFLICT);
+    }
+
     const doctor = await this.doctorsRepository.create({
       name,
       cellPhone,
