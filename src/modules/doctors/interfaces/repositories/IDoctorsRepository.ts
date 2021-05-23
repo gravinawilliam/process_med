a362764a4184a1bcd38eacfc1b2e303d1ea766e3
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-23 16:52:46 -0300

    feat: added method find by id and save in interface doctors repository

--- src/modules/doctors/interfaces/repositories/IDoctorsRepository.ts
+++ src/modules/doctors/interfaces/repositories/IDoctorsRepository.ts
@@ -5,4 +5,6 @@ export default interface IDoctorsRepository {
   create(doctor: ICreateDoctorDTO): Promise<IDoctor>;
   findByCellPhone(cellPhone: string): Promise<IDoctor | undefined>;
   findByCrm(crm: string): Promise<IDoctor | undefined>;
+  findById(id: string): Promise<IDoctor | undefined>;
+  save(doctor: IDoctor): Promise<IDoctor>;
 }
