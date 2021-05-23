86cc7f14deb106edf796aedb22727050c0945f6c
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-23 19:03:14 -0300

    feat: added method delete in interface doctors repository

--- src/modules/doctors/interfaces/repositories/IDoctorsRepository.ts
+++ src/modules/doctors/interfaces/repositories/IDoctorsRepository.ts
@@ -3,6 +3,7 @@ import IDoctor from '../models/IDoctor';
 
 export default interface IDoctorsRepository {
   create(doctor: ICreateDoctorDTO): Promise<IDoctor>;
+  delete(id: string): Promise<void>;
   findByCellPhone(cellPhone: string): Promise<IDoctor | undefined>;
   findByCrm(crm: string): Promise<IDoctor | undefined>;
   findById(id: string): Promise<IDoctor | undefined>;
