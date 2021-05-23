ce9ff766cc0efe4ee578632d0fb417371739b213
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-23 19:09:54 -0300

    test: must be able to delete a doctor

---
+++ src/modules/doctors/services/__tests__/DeleteDoctorService.spec.ts
@@ -0,0 +1,39 @@
+import FakeDoctorsRepository from '@modules/doctors/fakes/FakeDoctorsRepository';
+import FakeSpecialtiesRepository from '@modules/specialties/fakes/FakeSpecialtiesRepository';
+import DeleteDoctorService from '../DeleteDoctorService';
+
+let fakeDoctorsRepository: FakeDoctorsRepository;
+let fakeSpecialtiesRepository: FakeSpecialtiesRepository;
+let deleteDoctor: DeleteDoctorService;
+
+describe('Delete Doctor', () => {
+  beforeEach(() => {
+    fakeDoctorsRepository = new FakeDoctorsRepository();
+    fakeSpecialtiesRepository = new FakeSpecialtiesRepository();
+    deleteDoctor = new DeleteDoctorService(fakeDoctorsRepository);
+  });
+
+  it('must be able to delete a doctor', async () => {
+    const specialty1 = await fakeSpecialtiesRepository.create({
+      name: 'Alergologia',
+    });
+    const specialty2 = await fakeSpecialtiesRepository.create({
+      name: 'Angiologia',
+    });
+
+    const doctor = await fakeDoctorsRepository.create({
+      name: 'William',
+      cellPhone: '(32) 99833-8853',
+      cep: '36503-312',
+      crm: '33.225.11',
+      landline: '(32) 3532-2280',
+      specialties: [specialty1, specialty2],
+    });
+
+    const updatedDoctor = await deleteDoctor.execute({
+      doctorId: doctor.id,
+    });
+
+    expect(updatedDoctor);
+  });
+});
