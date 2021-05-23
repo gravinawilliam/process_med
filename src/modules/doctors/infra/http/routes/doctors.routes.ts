f0f6f4613f8e1f1209d7047f1a6f61af5fa7a388
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-23 17:03:03 -0300

    feat: route created for update doctor

--- src/modules/doctors/infra/http/routes/doctors.routes.ts
+++ src/modules/doctors/infra/http/routes/doctors.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import DoctorsController from '../controllers/DoctorsController';
 import createDoctorValidator from '../validators/createDoctor';
+import updateDoctorValidator from '../validators/updateDoctor';
 
 const doctorsRouter = Router();
 const doctors = new DoctorsController();
 
 doctorsRouter.post('/', createDoctorValidator, doctors.create);
+
+doctorsRouter.put('/:id', updateDoctorValidator, doctors.update);
 
 export default doctorsRouter;
