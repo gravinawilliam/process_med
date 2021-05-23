038c5f3167fc9ea27e1fc54af1022be3cf73ec25
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-23 19:32:08 -0300

    feat: created route for delete doctor

--- src/modules/doctors/infra/http/routes/doctors.routes.ts
+++ src/modules/doctors/infra/http/routes/doctors.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import DoctorsController from '../controllers/DoctorsController';
 import createDoctorValidator from '../validators/createDoctor';
+import deleteDoctorValidator from '../validators/deleteDoctor';
 import updateDoctorValidator from '../validators/updateDoctor';
 
 const doctorsRouter = Router();
@@ -9,5 +10,7 @@ const doctors = new DoctorsController();
 doctorsRouter.post('/', createDoctorValidator, doctors.create);
 
 doctorsRouter.put('/:id', updateDoctorValidator, doctors.update);
+
+doctorsRouter.delete('/:id', deleteDoctorValidator, doctors.delete);
 
 export default doctorsRouter;
