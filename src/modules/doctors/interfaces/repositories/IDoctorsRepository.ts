27b1e082f1ed82e5534c4319f52664999d2c9c18
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-24 00:40:04 -0300

    feat: added method findDoctors in interface doctors repository

--- src/modules/doctors/interfaces/repositories/IDoctorsRepository.ts
+++ src/modules/doctors/interfaces/repositories/IDoctorsRepository.ts
@@ -1,4 +1,5 @@
 import ICreateDoctorDTO from '../dtos/ICreateDoctorDTO';
+import ISearchDoctorDTO from '../dtos/ISearchDoctorDTO';
 import IDoctor from '../models/IDoctor';
 
 export default interface IDoctorsRepository {
@@ -6,6 +7,13 @@ export default interface IDoctorsRepository {
   delete(id: string): Promise<void>;
   findByCellPhone(cellPhone: string): Promise<IDoctor | undefined>;
   findByCrm(crm: string): Promise<IDoctor | undefined>;
+  findDoctors({
+    cellPhone,
+    cep,
+    crm,
+    landline,
+    name,
+  }: ISearchDoctorDTO): Promise<IDoctor[]>;
   findById(id: string): Promise<IDoctor | undefined>;
   save(doctor: IDoctor): Promise<IDoctor>;
 }
