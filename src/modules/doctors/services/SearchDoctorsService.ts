7b3bb3c7a221e2049afc45e1d069e4642ea875d9
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-24 00:43:15 -0300

    feat: service created for search doctors

---
+++ src/modules/doctors/services/SearchDoctorsService.ts
@@ -0,0 +1,29 @@
+import { inject, injectable } from 'tsyringe';
+import ISearchDoctorDTO from '../interfaces/dtos/ISearchDoctorDTO';
+import IDoctor from '../interfaces/models/IDoctor';
+import IDoctorsRepository from '../interfaces/repositories/IDoctorsRepository';
+
+@injectable()
+export default class SearchDoctorsService {
+  constructor(
+    @inject('DoctorsRepository')
+    private doctorsRepository: IDoctorsRepository,
+  ) {}
+
+  public async execute({
+    name,
+    cellPhone,
+    cep,
+    crm,
+    landline,
+  }: ISearchDoctorDTO): Promise<IDoctor[]> {
+    const doctors = await this.doctorsRepository.findDoctors({
+      cellPhone,
+      cep,
+      crm,
+      landline,
+      name,
+    });
+    return doctors;
+  }
+}
