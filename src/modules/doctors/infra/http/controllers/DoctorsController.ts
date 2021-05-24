59b728f9504292ed1aa581149055ad1aafa616ce
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-24 00:43:46 -0300

    feat: controller created for search doctors

--- src/modules/doctors/infra/http/controllers/DoctorsController.ts
+++ src/modules/doctors/infra/http/controllers/DoctorsController.ts
@@ -5,6 +5,7 @@ import { CREATED, NO_CONTENT, OK } from '@shared/constants/HttpStatusCode';
 import CreateDoctorService from '@modules/doctors/services/CreateDoctorService';
 import UpdateDoctorService from '@modules/doctors/services/UpdateDoctorService';
 import DeleteDoctorService from '@modules/doctors/services/DeleteDoctorService';
+import SearchDoctorsService from '@modules/doctors/services/SearchDoctorsService';
 
 export default class DoctorsController {
   public async create(req: Request, res: Response): Promise<Response> {
@@ -21,6 +22,28 @@ export default class DoctorsController {
     return res.status(CREATED).json(classToClass(response));
   }
 
+  public async delete(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const deleteDoctor = container.resolve(DeleteDoctorService);
+    const response = await deleteDoctor.execute({
+      doctorId: id,
+    });
+    return res.status(NO_CONTENT).json(classToClass(response));
+  }
+
+  public async index(req: Request, res: Response): Promise<Response> {
+    const { cellPhone, cep, crm, landline, name } = req.query;
+    const searchDoctors = container.resolve(SearchDoctorsService);
+    const response = await searchDoctors.execute({
+      cellPhone: String(cellPhone),
+      cep: String(cep),
+      crm: String(crm),
+      landline: String(landline),
+      name: String(name),
+    });
+    return res.status(OK).json(classToClass(response));
+  }
+
   public async update(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
     const { name, cellPhone, cep, crm, landline, specialtiesIds } = req.body;
@@ -35,14 +58,5 @@ export default class DoctorsController {
       specialtiesIds,
     });
     return res.status(OK).json(classToClass(response));
-  }
-
-  public async delete(req: Request, res: Response): Promise<Response> {
-    const { id } = req.params;
-    const deleteDoctor = container.resolve(DeleteDoctorService);
-    const response = await deleteDoctor.execute({
-      doctorId: id,
-    });
-    return res.status(NO_CONTENT).json(classToClass(response));
   }
 }
