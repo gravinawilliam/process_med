745a27ab5f91fa3064a5b229d0bcc91d3818cdf2
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-24 11:55:46 -0300

    feat: route created for list specialties

--- src/modules/specialties/infra/http/routes/specialties.routes.ts
+++ src/modules/specialties/infra/http/routes/specialties.routes.ts
@@ -7,4 +7,6 @@ const specialties = new SpecialtiesController();
 
 specialtiesRouter.post('/', createSpecialtyValidator, specialties.create);
 
+specialtiesRouter.get('/', specialties.index);
+
 export default specialtiesRouter;
