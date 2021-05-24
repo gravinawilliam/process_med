ffef3ace1dd9a04d4013064df63777e08a90744f
William Gravina <gravinawilliam@viwcode.com.br>
2021-05-24 00:45:06 -0300

    feat: route created for search doctors

--- src/modules/doctors/infra/http/routes/doctors.routes.ts
+++ src/modules/doctors/infra/http/routes/doctors.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import DoctorsController from '../controllers/DoctorsController';
 import createDoctorValidator from '../validators/createDoctor';
 import deleteDoctorValidator from '../validators/deleteDoctor';
+import searchDoctorsValidator from '../validators/searchDoctors';
 import updateDoctorValidator from '../validators/updateDoctor';
 
 const doctorsRouter = Router();
@@ -12,5 +13,7 @@ doctorsRouter.post('/', createDoctorValidator, doctors.create);
 doctorsRouter.put('/:id', updateDoctorValidator, doctors.update);
 
 doctorsRouter.delete('/:id', deleteDoctorValidator, doctors.delete);
+
+doctorsRouter.get('/', searchDoctorsValidator, doctors.index);
 
 export default doctorsRouter;
